23184bc9077606b65325d2f2eb16c7d874b1e5d8
Marcelo Ferreira <pount@sapo.pt>
2018-03-27 14:33:38 +0100

    I can obtain the name of a team

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-myResults_file = requests.get("http://www.meusresultados.com/futebol/portugal/primeira-liga/")
+myResults_file = requests.get("http://www.maisfutebol.iol.pt/resultadoseclassificacoes/128/Portugal/Primeira-Liga")
 
-soup = BeautifulSoup(myResults_file.text, "html.parser")
+soup = BeautifulSoup(myResults_file.content, "html.parser")
 
-table_of_results = soup.find_all("table", class_="soccer")
+table = soup.find("table", class_="tableJogosClass")
+tr_tag = table.find("tr")
+team = list(tr_tag.find("td", class_="team").children)[1].get_text()
 
-test_id = soup.find("tr", id="g_1_tdTHicx5")
-print(table_of_results)
+print(team)
